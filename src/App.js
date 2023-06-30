e6eab3b359d3dfc87a2d0c46d7f3e770799c9fe1
avillagabriella <avillagabriella@outlook.com>
2023-06-29 23:09:30 -0300

    novas funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FiSearch } from 'react-icons/fi';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="title">Buscador CEP</h1>
+        
+        <div className="containerInput">
+          <input
+          type="text"
+          placeholder="Didite seu CEP..."
+          /> 
+
+          <button className="buttonSearch">
+            <FiSearch size={12} color="#FFF"/>
+          </button>  
+        </div>
+
+        <main className="main">
+          <h2>CEP: 22233344</h2>
+
+          <span>Rua Teste algum</span>
+          <span>Complemento: Algum complemento</span>
+          <span>Vila Teste</span>
+          <span>Teresina - PI</span>
+
+        </main>
     </div>
   );
 }
 
 export default App;
+
+
+
+// esse é o principal
